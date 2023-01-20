feacf8213fa137bc8ed8c520a68345fd3a3cfe4a
Mustafa Hassan <mustafahassan.dev@gmail.com>
2023-01-20 20:01:47 +0200

    Add todo controllers

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 app.get("/", todoController.homePage);
+app.post("/create", todoController.createTodo);
+app.get("/delete/:index", todoController.deleteTodo);
 
 app.listen(app.get("port"), () => {
   console.log(`Server running at http://localhost:${app.get("port")}`);
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,9 +1,25 @@
+const todos = ["Todo 2", "Todo 2", "Todo 3"];
+
 const homePage = (req, res) => {
-  res.render("index");
+  res.render("index", { todos });
+};
+
+const deleteTodo = (req, res) => {
+  const index = req.params.index;
+  todos.splice(index, 1);
+  res.redirect("/");
+};
+
+const createTodo = (req, res) => {
+  const todo = req.body.todo;
+  todos.push(todo);
+  res.redirect("/");
 };
 
 const todoController = {
   homePage,
+  deleteTodo,
+  createTodo,
 };
 
 export default todoController;
